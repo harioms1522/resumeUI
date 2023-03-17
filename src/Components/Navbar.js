473c2244528cee398a7e31d31472eaa3d206854a
hariom <harioms1522@gmail.com>
2023-03-17 08:23:57 +0530

    nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,12 @@
 import React from 'react'
-import homeIcon from "./icons/home.png"
-import facebookIcon from "./icons/fb-icon.png"
 import linkedIcon from "./icons/linkedin.png"
 import githubIcon from "./icons/github.png"
-import msgIcon from "./icons/hangout.png"
-
 // this is only visible when the screen size is small
 export default function Navbar() {
   return (
     <div className='navbar'>
-        <img src={homeIcon} alt="home"></img>
-        <img src={msgIcon} alt="home"></img>
-        <img src={linkedIcon} alt="home"></img>
-        <img src={githubIcon} alt="home"></img>
-        <img src={facebookIcon} alt="home"></img>
+        <a href='https://linkedin.com/in/harioms152'><img src={linkedIcon} alt="linkedin" /></a>
+        <a href='https://github.com/harioms1522'><img src={githubIcon} alt="linkedin"/></a>
     </div>
   )
 }
